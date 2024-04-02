df6a5689904c00d3d397e4c618cab35760597d53
Kiruthikaece <102378821+Kiruthikaece@users.noreply.github.com>
2024-04-02 17:25:37 +0530

    Update CustomException.java

--- Exception_handling_Assignment/CustomException.java
+++ Exception_handling_Assignment/CustomException.java
@@ -1,22 +1,28 @@
 import java.util.Scanner;
 
 public class CustomException {
-  public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter number");
-    int n=sc.nextInt();
-    CheckVoter(n);
-  }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age:");
+        int age = sc.nextInt();
+        try {
+            checkVoter(age);
+            System.out.println("You are eligible for voting.");
+        } catch (AgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-private static void CheckVoter(int n) {
-   try {
-    if(n<18)
-    throw new Exception("You are not eligible for voting");
-    else
-    System.out.println("you are age is"+n+"elgible for voting");
-   }
-   catch(Exception e) {
-    System.out.println(e.getMessage());
-   }
+    private static void checkVoter(int age) throws AgeException {
+        if (age < 18)
+            throw new AgeException("You are not eligible for voting");
+        else
+            System.out.println("Your age is " + age + ", you are eligible for voting.");
+    }
 }
+
+class AgeException extends Exception {
+    AgeException(String message) {
+        super(message);
+    }
 }
